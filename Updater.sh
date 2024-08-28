748f712aa13e75ea3fc57cf91bd0700ec351e788
nall-nifmcp <n.allaire@nifmcp.com>
2024-08-28 10:34:14 -0400

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -1,2 +1,14 @@
 #Check for updates
+CurrentVersion=$(cat /boot/lib/version)
+wget -N https://raw.githubusercontent.com/nall-nifmcp/AutoSpeedTest10ZigConfig/main/Version
+NewVersion=$(cat /boot/lib/version)
+
+if ["$CurrentVersion" != "$NewVersion"]
+then
+  echo Update is available...
+  echo Updating....
+  cd /boot/autostart.d
+  wget -N https://raw.githubusercontent.com/nall-nifmcp/AutoSpeedTest10ZigConfig/main/SpeedTestConfig.sh
+  chmod +x /boot/autostart.d/SpeedTestConfig.sh  
+fi
 
