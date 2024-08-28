51c2d318414d73b230a8ab278d68c06e1af119a3
nall-nifmcp <n.allaire@nifmcp.com>
2024-08-28 11:00:39 -0400

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -10,5 +10,8 @@ then
   cd /boot/autostart.d
   wget -q https://raw.githubusercontent.com/nall-nifmcp/AutoSpeedTest10ZigConfig/main/SpeedTestConfig.sh -O /boot/autostart.d/SpeedTestConfig.sh
   chmod +x /boot/autostart.d/SpeedTestConfig.sh  
+  echo "Update Completed from $CurrentVersion to $NewVersion"
+else
+echo "version $CurrentVersion is up-to-date"
 fi
 
